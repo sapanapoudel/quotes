388af695828a7930085b8c4c4491a1550c8003a4
sapanapoudel <sapana.poudel17@outlook.com>
2019-08-15 11:28:13 -0700

    tests for toString and file reader

--- src/main/java/quotes/App.java
+++ src/main/java/quotes/App.java
@@ -3,6 +3,7 @@ package quotes;
 public class App {
     public static void main(String[] args){
         Parser parser = new Parser();
-        parser.jsonReader();
+        Quotes[] quotes = parser.jsonReader();
+        parser.randomQuote(quotes);
     }
 }
