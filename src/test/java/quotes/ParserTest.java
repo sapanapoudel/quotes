7443d912e5364f7cb58e3d99545df42edf8c070c
Nicholas Paro <nichparo@amazon.com>
2019-08-15 11:50:51 -0700

    added test verbosity

--- src/test/java/quotes/ParserTest.java
+++ src/test/java/quotes/ParserTest.java
@@ -14,4 +14,24 @@ public class ParserTest {
             "Quote:  “I am good, but not an angel. I do sin, but I am not the devil. I am just a small girl in a big world trying to find someone to love.” \n",
             quotes[0].toString());
   }
+
+  @Test
+  public void testRandomNum() {
+    Parser parser = new Parser();
+    Quotes[] quotes = parser.jsonReader();
+    for (int i = 0; i < 1000; i++) {
+      int randomNum = parser.randomNum(quotes);
+      assertTrue(randomNum >= 0 && randomNum < quotes.length);
+    }
+  }
+
+  @Test
+  public void testPrintQuote() {
+    Parser parser = new Parser();
+    Quotes[] quotes = parser.jsonReader();
+    for (int i = 0; i < 1000; i++) {
+      String quote = parser.printQuote(quotes);
+      assertNotNull(quote);
+    }
+  }
 }
